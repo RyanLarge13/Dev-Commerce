6a5bb342e660b8c2061e3903b65672b5f215af90
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-10-17 21:12:08 -0700

    Updating file structure for easy scaling.

--- Views/JavaScript/maps.js
+++
@@ -1,7 +0,0 @@
-function myMap() {
-  let mapProp= {
-    center: new google.maps.LatLng(51.508742,-0.120850),
-    zoom: 5,
-  };
-  let map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
