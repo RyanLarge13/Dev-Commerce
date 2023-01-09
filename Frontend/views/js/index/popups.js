362820c9125d1ba01ca33e901e3fda0e212b0949
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-01-09 00:45:46 -0800

    Updating the UI to include post data and styles for each blog post.

--- Frontend/views/js/index/popups.js
+++ Frontend/views/js/index/popups.js
@@ -3,6 +3,15 @@ export const showLogin = () => {
   const text = popup.querySelector("p");
   popup.classList.add("show");
   text.innerText = "Login here!";
+  setTimeout(() => {
+    popup.style.transform = "scale(1)";
+  }, 1);
+  setTimeout(() => {
+    popup.style.transform = "scale(0)";
+  }, 3000);
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 3300);
 };
 
 // window.addEventListener("DOMContentLoaded", showLogin);
